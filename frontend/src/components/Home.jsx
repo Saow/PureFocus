e8e3c2a4d2a8927030f51c58dabf19c173b3063e
samuelnummela <samuelnummela06@gmail.com>
2024-05-23 12:15:04 +0300

    timer and slider stopper

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Home() {
   const [totalAppOpenings, setTotalAppOpenings] = useState(0);
@@ -8,7 +8,7 @@ function Home() {
 
   useEffect(() => {
     // Check if localStorage has a value for totalAppOpenings
-    const storedOpenings = localStorage.getItem('totalAppOpenings');
+    const storedOpenings = localStorage.getItem("totalAppOpenings");
     const openings = storedOpenings ? parseInt(storedOpenings) : 0;
     setTotalAppOpenings(openings);
 
@@ -16,7 +16,7 @@ function Home() {
     let interval;
     if (timerRunning) {
       interval = setInterval(() => {
-        setTimerSeconds(prevSeconds => {
+        setTimerSeconds((prevSeconds) => {
           if (prevSeconds <= 0) {
             clearInterval(interval);
             setTimerRunning(false);
@@ -37,7 +37,7 @@ function Home() {
   const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
-    return `${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+    return `${mins < 10 ? "0" : ""}${mins}:${secs < 10 ? "0" : ""}${secs}`;
   };
 
   const handleSliderChange = (event) => {
@@ -46,38 +46,67 @@ function Home() {
   };
 
   const handleSliderRelease = () => {
-    if (sliderPosition === 100) {
+    if (sliderPosition) {
+      setSliderPosition(0);
+    }
+    if (sliderPosition > 90) {
       setSliderPosition(0); // Reset slider position to 0%
       setTimerSeconds(0); // Reset timer
       setTimerRunning(false); // Stop timer
     }
   };
-  
-  
 
   return (
     <div>
-      <div className='bg-gray-100'>
-        <div className='p-5 h-52'>
-          <h1 className='text-2xl mb-2 font-serif'>Welcome to keep your<br/><i>Focus pure</i></h1>
-          <p><b className='text-xl'>{totalAppOpenings}</b><br/> Total Openings</p>
+      <div className="bg-gray-100">
+        <div className="p-7 h-52">
+          <h1 className="text-2xl mb-2 font-serif">
+            Welcome to keep your
+            <br />
+            <i>Focus pure</i>
+          </h1>
+          <p>
+            <b className="text-xl">{totalAppOpenings}</b>
+            <br /> Total Openings
+          </p>
         </div>
       </div>
       <main>
-        <p className='flex flex-col items-center text-center p-40'>Timer: {formatTime(timerSeconds)}</p>
-        <div className='flex flex-row justify-center gap-5'>
-          <button className='bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all' onClick={() => startTimer(300)}>5 Minutes</button>
-          <button className='bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all' onClick={() => startTimer(900)}>15 Minutes</button>
-          <button className='bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all' onClick={() => startTimer(1500)}>25 Minutes</button>
+        <p className="flex flex-col items-center text-center p-32 text-5xl">
+          {formatTime(timerSeconds)}
+        </p>
+        <div className="flex flex-row justify-center gap-5">
+          <button
+            className="bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all"
+            onClick={() => startTimer(300)}
+          >
+            5 Minutes
+          </button>
+          <button
+            className="bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all"
+            onClick={() => startTimer(900)}
+          >
+            15 Minutes
+          </button>
+          <button
+            className="bg-blue-400 p-2 rounded-md hover:bg-blue-500 transition-all"
+            onClick={() => startTimer(1500)}
+          >
+            25 Minutes
+          </button>
         </div>
         <div className="flex justify-center items-center mt-8">
           <input
+            className="w-4/6 appearance-none bg-gray-200 h-12 rounded-full outline-none"
             type="range"
             min="0"
             max="100"
             value={sliderPosition}
             onChange={handleSliderChange}
             onMouseUp={handleSliderRelease}
+            style={{
+              background: `linear-gradient(to right, #3182ce 10%, #3182ce ${sliderPosition}%, #d1d5db ${sliderPosition}%, #d1d5db 100%)`,
+            }}
           />
         </div>
       </main>
